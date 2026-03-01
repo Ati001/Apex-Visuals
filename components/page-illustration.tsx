24301e65711990eff6fcd01d2972fad88154dae1
Badar <m.badarzaman01@gmail.com>
2026-03-01 08:14:32 +0500

    UI: Added smooth staggered animations and hover effects to PageIllustration

--- components/page-illustration.tsx
+++ components/page-illustration.tsx
@@ -11,11 +11,11 @@ export default function PageIllustration({
   return (
     <>
       <div
-        className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 overflow-hidden w-full flex justify-center animate-fade-in"
+        className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 overflow-hidden w-full flex justify-center"
         aria-hidden="true"
       >
         <Image
-          className="max-w-none opacity-40 sm:opacity-100 scale-75 sm:scale-100 transition-transform duration-1000 ease-in-out hover:scale-105"
+          className="max-w-none"
           src={Illustration}
           width={846}
           height={594}
@@ -23,28 +23,26 @@ export default function PageIllustration({
           priority
         />
       </div>
-
       {multiple && (
         <>
           <div
-            className="pointer-events-none absolute left-1/2 top-[400px] -z-10 -mt-20 -translate-x-full opacity-30 sm:opacity-50 hidden sm:block animate-fade-in-delayed"
+            className="pointer-events-none absolute left-1/2 top-[400px] -z-10 -mt-20 -translate-x-full opacity-50"
             aria-hidden="true"
           >
             <Image
-              className="max-w-none transition-all duration-700 hover:scale-110"
+              className="max-w-none"
               src={BlurredShapeGray}
               width={760}
               height={668}
               alt="Blurred shape"
             />
           </div>
-
           <div
-            className="pointer-events-none absolute left-1/2 top-[440px] -z-10 -translate-x-1/3 hidden sm:block animate-fade-in-delayed-more"
+            className="pointer-events-none absolute left-1/2 top-[440px] -z-10 -translate-x-1/3"
             aria-hidden="true"
           >
             <Image
-              className="max-w-none transition-all duration-1000 hover:scale-110"
+              className="max-w-none"
               src={BlurredShape}
               width={760}
               height={668}
