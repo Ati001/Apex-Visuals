89427358ccc552572101d31621fe65b79231693e
Badar <m.badarzaman01@gmail.com>
2026-03-01 08:02:36 +0500

    Fix: Synchronized animations and cleared CSS warnings

--- components/page-illustration.tsx
+++ components/page-illustration.tsx
@@ -10,13 +10,12 @@ export default function PageIllustration({
 }) {
   return (
     <>
-      {/* Primary Illustration with Entry Animation */}
       <div
         className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 overflow-hidden w-full flex justify-center animate-fade-in"
         aria-hidden="true"
       >
         <Image
-          className="max-w-none opacity-40 sm:opacity-100 scale-75 sm:scale-100 transition-transform duration-1000 ease-in-out hover:scale-110"
+          className="max-w-none opacity-40 sm:opacity-100 scale-75 sm:scale-100 transition-transform duration-1000 ease-in-out hover:scale-105"
           src={Illustration}
           width={846}
           height={594}
@@ -27,13 +26,12 @@ export default function PageIllustration({
 
       {multiple && (
         <>
-          {/* Gray Blurred Shape - Staggered Fade */}
           <div
             className="pointer-events-none absolute left-1/2 top-[400px] -z-10 -mt-20 -translate-x-full opacity-30 sm:opacity-50 hidden sm:block animate-fade-in-delayed"
             aria-hidden="true"
           >
             <Image
-              className="max-w-none transition-all duration-700 hover:scale-110 hover:rotate-3"
+              className="max-w-none transition-all duration-700 hover:scale-110"
               src={BlurredShapeGray}
               width={760}
               height={668}
@@ -41,13 +39,12 @@ export default function PageIllustration({
             />
           </div>
 
-          {/* Color Blurred Shape - Staggered Fade */}
           <div
             className="pointer-events-none absolute left-1/2 top-[440px] -z-10 -translate-x-1/3 hidden sm:block animate-fade-in-delayed-more"
             aria-hidden="true"
           >
             <Image
-              className="max-w-none transition-all duration-1000 hover:scale-125 hover:-rotate-3"
+              className="max-w-none transition-all duration-1000 hover:scale-110"
               src={BlurredShape}
               width={760}
               height={668}
